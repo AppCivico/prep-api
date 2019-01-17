993f7fa391b26be8ac364b0eb7505f01bcde20c4
Lucas Ansei <lucas.ansei@eokoe.com>
2019-01-17 18:10:44 -0200

    Updating gitignore

---
+++ envfile.sh
@@ -0,0 +1,9 @@
+export SQITCH_DEPLOY="docker"
+
+export POSTGRESQL_HOST="172.17.0.1"
+export POSTGRESQL_PORT="5432"
+export POSTGRESQL_DBNAME="prep_dev"
+export POSTGRESQL_USER="postgres"
+export POSTGRESQL_PASSWORD="trust"
+
+export CHATBOT_SECURITY_TOKEN=""
