50805494877a85a69d34b2a7225b3d03898e8f89
Lucas Ansei <lucas.ansei@eokoe.com>
2019-01-18 11:31:42 -0200

    Fix: use prep_dev on envfile.sh

--- envfile.sh
+++ envfile.sh
@@ -2,7 +2,7 @@ export SQITCH_DEPLOY="docker"
 
 export POSTGRESQL_HOST="172.17.0.1"
 export POSTGRESQL_PORT="5432"
-export POSTGRESQL_DBNAME="prep_dev_persistent"
+export POSTGRESQL_DBNAME="prep_dev"
 export POSTGRESQL_USER="postgres"
 export POSTGRESQL_PASSWORD="trust"
 
