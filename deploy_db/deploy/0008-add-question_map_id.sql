d20d1780ffd240bded09cf31081ef6394cd01bda
Lucas Ansei <lucas.ansei@eokoe.com>
2019-01-29 17:42:17 -0200

    Fix: sqitch

--- deploy_db/deploy/0008-add-question_map_id.sql
+++ deploy_db/deploy/0008-add-question_map_id.sql
@@ -4,7 +4,7 @@
 BEGIN;
 
 ALTER TABLE question ADD COLUMN question_map_id INTEGER REFERENCES question_map(id);
-UPDATE question SET question_map_id = 1;
+UPDATE question SET question_map_id = 2;
 ALTER TABLE question ALTER COLUMN question_map_id SET NOT NULL;
 ALTER TABLE question DROP CONSTRAINT question_code_key;
 
