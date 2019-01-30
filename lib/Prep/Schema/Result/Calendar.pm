b6324b3a3320f1020c7a5408878e2d741d228f91
Lucas Ansei <lucas.ansei@eokoe.com>
2019-01-30 14:17:39 -0200

    Fix: time zone

--- lib/Prep/Schema/Result/Calendar.pm
+++ lib/Prep/Schema/Result/Calendar.pm
@@ -270,9 +270,9 @@ sub available_dates {
                                     ( $start_time->hms . ' - ' . $start_time->add($seconds_per_quota * $_ )->hms ) :
                                     ( $start_time->add($seconds_per_quota * ($_ - 1))->hms . ' - ' . $start_time->add($seconds_per_quota * $_)->hms ),
                                 datetime_start => $_ == 1 ?
-                                    ( $ymd . 'T' . $start_time->hms . '+00:00' ) :
-                                    ( $ymd . 'T' . $start_time->add($seconds_per_quota * ($_ - 1))->hms . '+00:00' ),
-                                datetime_end => $ymd . 'T' . $start_time->add($seconds_per_quota * $_ )->hms . '+00:00'
+                                    ( $ymd . 'T' . $start_time->hms . '-02:00' ) :
+                                    ( $ymd . 'T' . $start_time->add($seconds_per_quota * ($_ - 1))->hms . '-02:00' ),
+                                datetime_end => $ymd . 'T' . $start_time->add($seconds_per_quota * $_ )->hms . '-02:00'
                             }
                         } @base_quotas
                     ]
