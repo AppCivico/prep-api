0cf2b4c6b9b46186da3c1d9c2ec267eb52e9879f
Lucas Ansei <lucas.ansei@eokoe.com>
2019-02-11 14:57:03 -0200

    Adding flags to delete answer

--- lib/Prep/Controller/Internal/DeleteAnswer.pm
+++ lib/Prep/Controller/Internal/DeleteAnswer.pm
@@ -15,7 +15,16 @@ sub post {
     my $recipient       = $c->schema->resultset('Recipient')->search( { fb_id => $recipient_fb_id } )->next
       or die \['fb_id', 'invalid'];
 
-    eval { $recipient->answers->delete; $recipient->update( { finished_quiz => 0 } ) };
+    eval {
+        $recipient->answers->delete;
+        $recipient->update(
+            {
+                finished_quiz          => 0,
+                count_sent_quiz        => 0,
+                count_invited_research => 0
+            }
+        );
+    };
 
     return $c->render(
         status => 200,
