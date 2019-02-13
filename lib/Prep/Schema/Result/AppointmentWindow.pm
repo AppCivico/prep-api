ff8e5331d9f0bfa1a43bb76b5d0c439ac8bac145
Lucas Ansei <lucas.ansei@eokoe.com>
2019-02-13 17:23:42 -0200

    Adding logs

--- lib/Prep/Schema/Result/AppointmentWindow.pm
+++ lib/Prep/Schema/Result/AppointmentWindow.pm
@@ -254,6 +254,8 @@ sub assert_quota_number {
 
     my $selected_quota = $quota_map->{ $opts{quota_number} } or die \['quota_number', 'invalid'];
     use DDP; p $selected_quota->{start}; p $start_time->hms;
+	print STDERR $selected_quota->{start};
+	print STDERR $start_time->hms;
 	die \[ 'datetime_start', 'invalid' ] unless $selected_quota->{start} eq $start_time->hms;
 	die \[ 'datetime_end',   'invalid' ] unless $selected_quota->{end}   eq $end_time->hms;
 }
