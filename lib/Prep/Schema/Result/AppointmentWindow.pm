5f933bf051956057babf485ba0c5760367c2b77f
Lucas Ansei <lucas.ansei@eokoe.com>
2019-02-13 17:25:22 -0200

    Improving error message

--- lib/Prep/Schema/Result/AppointmentWindow.pm
+++ lib/Prep/Schema/Result/AppointmentWindow.pm
@@ -256,8 +256,8 @@ sub assert_quota_number {
     use DDP; p $selected_quota->{start}; p $start_time->hms;
 	print STDERR $selected_quota->{start};
 	print STDERR $start_time->hms;
-	die \[ 'datetime_start', 'invalid' ] unless $selected_quota->{start} eq $start_time->hms;
-	die \[ 'datetime_end',   'invalid' ] unless $selected_quota->{end}   eq $end_time->hms;
+	die \[ 'datetime_start', 'does not matches quota start time' ] unless $selected_quota->{start} eq $start_time->hms;
+	die \[ 'datetime_end',   'does not matches quota end time' ] unless $selected_quota->{end}   eq $end_time->hms;
 }
 
 __PACKAGE__->meta->make_immutable;
