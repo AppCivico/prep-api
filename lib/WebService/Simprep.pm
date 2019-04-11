6ff2691d12d37aa26fc25cea583a4cb540992e47
Lucas Ansei <lucastakushi@hotmail.com>
2019-04-11 16:44:28 -0300

    Adding simprep webservice

---
+++ lib/WebService/Simprep.pm
@@ -0,0 +1,93 @@
+package WebService::Simprep;
+use common::sense;
+use MooseX::Singleton;
+
+use JSON;
+use LWP::UserAgent;
+use Try::Tiny::Retry;
+use Prep::Utils;
+
+has 'ua' => ( is => 'rw', lazy => 1, builder => '_build_ua' );
+
+sub _build_ua { LWP::UserAgent->new() }
+
+sub register_recipient {
+    my ( $self, %opts ) = @_;
+
+	my @required_opts = qw( voucher answers );
+	defined $opts{$_} or die \["opts{$_}", 'missing'] for @required_opts;
+
+    if (is_test()) {
+        return {
+            voucher => $opts{voucher},
+            url     => 'https://www.google.com'
+        };
+    }
+    else {
+        my $res;
+
+        eval {
+            retry {
+                my $url = $ENV{SIMPREP_API_URL} . '/register';
+
+                $res = $self->ua->post(
+                    $url,
+                    Content_Type => 'application/json',
+                    Content      => encode_json(
+                        {
+                            voucher => $opts{voucher},
+                            answers => $opts{answers}
+                        }
+                    )
+                );
+                die $res->decoded_content unless $res->is_success;
+
+                my $response = decode_json( $res->decoded_content );
+                die \['file', 'invalid response'] unless $response->{attachment_id};
+
+            }
+            retry_if { shift() < 3 } catch { die $_; };
+        };
+        die $@ if $@;
+
+        return decode_json( $res->decoded_content );
+    }
+}
+
+sub get_form_for_notification {
+    my ($self, %opts) = @_;
+
+    my @required_opts = qw( voucher );
+    defined $opts{$_} or die \["opts{$_}", 'missing'] for @required_opts;
+
+    if (is_test()) {
+        return {
+            url     => 'https://www.google.com',
+            voucher => $opts{voucher}
+        };
+    }
+    else {
+        my $res;
+        eval {
+            retry {
+                my $url = $ENV{SIMPREP_API_URL} . '/form';
+
+                $res = $self->ua->get(
+                    $url
+                );
+
+                die $res->decoded_content unless $res->is_success;
+
+                my $response = decode_json( $res->decoded_content );
+                die 'invalid response' unless $response;
+
+            }
+            retry_if { shift() < 3 } catch { die $_; };
+        };
+        die $@ if $@;
+
+        return decode_json( $res->decoded_content );
+    }
+}
+
+1;
