ee3059189cc30995c67c4e8d07033cfcc7fd16e0
Lucas Ansei <lucastakushi@hotmail.com>
2019-05-09 15:03:49 -0300

    Adding log

--- lib/Prep/Controller/Internal.pm
+++ lib/Prep/Controller/Internal.pm
@@ -7,9 +7,6 @@ sub validade_security_token {
     my $c = shift;
 
     my $security_token = env('INTERNAL_SECURITY_TOKEN');
-    my $foo = $c->req->params->to_hash->{security_token};
-	$c->app->log->debug("================================================================================  $foo ================================================================================");
-	$c->app->log->info("================================================================================  $foo ================================================================================");
 
     if ( !$c->req->params->to_hash->{security_token} || $security_token ne $c->req->params->to_hash->{security_token} ) {
         $c->reply_forbidden();
