21e8a9b56c1f67ae0c0401b6a11a0a30b310ca9c
Lucas Ansei <lucastakushi@hotmail.com>
2019-05-21 15:28:32 -0300

    Updating envfile

--- envfile.sh
+++ envfile.sh
@@ -16,6 +16,6 @@ export REFRESH_TOKEN=""
 export GOOGLE_CALENDAR_API_URL="https://www.googleapis.com/calendar/v3"
 
 export SIMPREP_API_URL="http://206.189.170.15/api"
-export SIMPREP_TOKEN=""
+export SIMPREP_TOKEN="f1ed2d512acf7525c3a8244091711b4b16b9f33e"
 
 export FB_API_URL="https://graph.facebook.com/v3.2"
