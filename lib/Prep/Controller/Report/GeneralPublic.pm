797fba30445601a98d98f9e8d81c174fc543384d
Lucas Ansei <lucas.ansei@eokoe.com>
2020-02-19 10:52:35 -0300

    Tidy up

--- lib/Prep/Controller/Report/GeneralPublic.pm
+++ lib/Prep/Controller/Report/GeneralPublic.pm
@@ -33,18 +33,18 @@ sub get {
 
         my ($label, $value, $weight);
         if ($key eq 'count_finished_publico_interesse') {
-            $label = 'Finalizaram bloco A';
-            $value = $view->count_finished_publico_interesse;
+            $label  = 'Finalizaram bloco A';
+            $value  = $view->count_finished_publico_interesse;
             $weight = 6;
         }
         elsif ($key eq 'count_finished_quiz_brincadeira') {
-            $label = 'Finalizaram quiz de brincadeira';
-            $value = $view->count_finished_quiz_brincadeira;
+            $label  = 'Finalizaram quiz de brincadeira';
+            $value  = $view->count_finished_quiz_brincadeira;
             $weight = 7;
         }
         elsif ($key eq 'count_multiple_interactions') {
-            $label = 'Possuem mais de uma interação';
-            $value = $view->count_multiple_interactions;
+            $label  = 'Possuem mais de uma interação';
+            $value  = $view->count_multiple_interactions;
             $weight = 2;
         }
         elsif ($key eq 'count_one_interaction') {
@@ -53,23 +53,23 @@ sub get {
             $weight = 1;
         }
         elsif ($key eq 'count_refused_publico_interesse') {
-            $label = 'Apertaram "agora não" no menu principal';
-            $value = $view->count_refused_publico_interesse;
+            $label  = 'Apertaram "agora não" no menu principal';
+            $value  = $view->count_refused_publico_interesse;
             $weight = 3;
         }
         elsif ($key eq 'count_started_publico_interesse') {
-            $label = 'Começaram o bloco A';
-            $value = $view->count_started_publico_interesse;
+            $label  = 'Começaram o bloco A';
+            $value  = $view->count_started_publico_interesse;
             $weight = 5;
         }
         elsif ($key eq 'count_started_publico_interesse_after_refusal') {
-            $label = 'Começaram o bloco A após apertar "agora não" no menu principal';
-            $value = $view->count_started_publico_interesse_after_refusal;
+            $label  = 'Começaram o bloco A após apertar "agora não" no menu principal';
+            $value  = $view->count_started_publico_interesse_after_refusal;
             $weight = 4;
         }
         else {
-            $label = 'Começaram o quiz de brincadeira';
-            $value = $view->count_started_quiz_brincadeira;
+            $label  = 'Começaram o quiz de brincadeira';
+            $value  = $view->count_started_quiz_brincadeira;
             $weight = 8;
         }
 
