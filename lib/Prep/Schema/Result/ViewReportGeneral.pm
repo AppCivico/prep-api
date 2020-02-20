2a5fc874ef1fb8f5a056236b9f0170b27de105e9
Lucas Ansei <lucas.ansei@eokoe.com>
2020-02-20 14:57:27 -0300

    Using join instead of exists

--- lib/Prep/Schema/Result/ViewReportGeneral.pm
+++ lib/Prep/Schema/Result/ViewReportGeneral.pm
@@ -62,10 +62,7 @@ SELECT
             count(1)
         FROM
             answers_filtered a
-        WHERE
-            EXISTS(
-                SELECT 1 FROM quick_reply_log_grouped q WHERE a.recipient_id = q.recipient_id
-            )
+        JOIN quick_reply_log_grouped q ON q.recipient_id = a.recipient_id
     ) AS count_started_publico_interesse_after_refusal,
     ( SELECT COUNT(DISTINCT recipient_id) FROM answers_filtered ) AS count_started_publico_interesse,
     (
