fe45bd2903203322b97fe89e4a573a3d3ee6a946
Lucas Ansei <lucas.ansei@eokoe.com>
2020-02-20 15:06:26 -0300

    Inverting join

--- lib/Prep/Schema/Result/ViewReportGeneral.pm
+++ lib/Prep/Schema/Result/ViewReportGeneral.pm
@@ -61,8 +61,8 @@ SELECT
         SELECT
             count(1)
         FROM
-            answers_filtered a
-        JOIN quick_reply_log_grouped q ON q.recipient_id = a.recipient_id
+            quick_reply_log_grouped q
+        JOIN answer a ON q.recipient_id = a.recipient_id
     ) AS count_started_publico_interesse_after_refusal,
     ( SELECT COUNT(DISTINCT recipient_id) FROM answers_filtered ) AS count_started_publico_interesse,
     (
