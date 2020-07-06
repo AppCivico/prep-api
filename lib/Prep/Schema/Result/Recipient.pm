5bede399a5fef2eca123268ae7c065e849c6c0a2
Lucas <lucas.ansei@appcivico.com>
2020-07-06 16:04:15 -0300

    Save message

--- lib/Prep/Schema/Result/Recipient.pm
+++ lib/Prep/Schema/Result/Recipient.pm
@@ -2215,7 +2215,7 @@ sub update_sisprep {
 
             $recipient_integration->update(
                 {
-                    err_msg => length $@ > 1000 ? 'err_msg_too_big' : $@,
+                    err_msg => $@,
                     next_retry_at => \"NOW() + interval '4 hours'"
                 }
             );
